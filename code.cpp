1fb722d5e3b5324489dd54765c3a2349fb7ffaf8
unknown <rafaulgoni787898@gmail.com>
2024-12-31 22:20:57 +0600

    done one

--- code.cpp
+++ code.cpp
@@ -44,3 +44,85 @@ int main() {
 //         return head;
 //     }
 // };
+
+// class MyLinkedList {
+// private:
+//     ListNode* head;
+//     int size;
+
+// public:
+//     MyLinkedList() {
+//         head = NULL;
+//         size = 0;
+//     }
+    
+//     int get(int index) {
+//         if (index < 0 || index >= size) {
+//             return -1;
+//         }
+//         ListNode* current = head;
+//         for (int i = 0; i < index; ++i) {
+//             current = current->next;
+//         }
+//         return current->val;
+//     }
+    
+//     void addAtHead(int val) {
+//         ListNode* newNode = new ListNode(val);
+//         newNode->next = head;
+//         head = newNode;
+//         ++size;
+//     }
+    
+//     void addAtTail(int val) {
+//         ListNode* newNode = new ListNode(val);
+//         if (head == NULL) {
+//             head = newNode;
+//         } else {
+//             ListNode* current = head;
+//             while (current->next != NULL) {
+//                 current = current->next;
+//             }
+//             current->next = newNode;
+//         }
+//         ++size;
+//     }
+    
+//     void addAtIndex(int index, int val) {
+//         if (index < 0 || index > size) {
+//             return;
+//         }
+//         if (index == 0) {
+//             addAtHead(val);
+//         } else {
+//             ListNode* newNode = new ListNode(val);
+//             ListNode* current = head;
+//             for (int i = 0; i < index - 1; ++i) {
+//                 current = current->next;
+//             }
+//             newNode->next = current->next;
+//             current->next = newNode;
+//             ++size;
+//         }
+//     }
+    
+//     void deleteAtIndex(int index) {
+//         if (index < 0 || index >= size) {
+//             return;
+//         }
+//         if (index == 0) {
+//             ListNode* temp = head;
+//             head = head->next;
+//             delete temp;
+//         } else {
+//             ListNode* current = head;
+//             for (int i = 0; i < index - 1; ++i) {
+//                 current = current->next;
+//             }
+//             ListNode* temp = current->next;
+//             current->next = temp->next;
+//             delete temp;
+//         }
+//         --size;
+//     }
+// };
