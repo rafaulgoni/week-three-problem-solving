53df9dde4271fdf9e992b558b66569690a54b8c4
unknown <rafaulgoni787898@gmail.com>
2025-01-02 00:20:04 +0600

    problem done

--- code.cpp
+++ code.cpp
@@ -143,3 +143,36 @@ int main() {
 //         return list1;
 //     }
 // };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// class Solution {
+// public:
+//     ListNode* swapPairs(ListNode* head) {
+//         ListNode dummy(0);
+//         dummy.next = head;
+//         ListNode* current = &dummy;
+
+//         while (current->next && current->next->next) {
+//             ListNode* first = current->next;
+//             ListNode* second = current->next->next;
+
+//             first->next = second->next;
+//             second->next = first;
+//             current->next = second;
+
+//             current = first;
+//         }
+
+//         return dummy.next;
+//     }
+// };
